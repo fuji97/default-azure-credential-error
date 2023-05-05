e4fba0a10db82bb0afaaccdf718895a20f45c656
Federico Rapetti <fuji1097@gmail.com>
2023-05-05 11:10:35 +0200

    Initial commit

---
+++ Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => {
+        services.AddAzureClients(builder => {
+            builder.UseCredential(new DefaultAzureCredential());
+            builder.AddClient<ArmClient, ArmClientOptions>((options, creds) => new ArmClient(creds));
+        });
+    })
+    .Build();
+
+var armClient = new ArmClient(new DefaultAzureCredential());
+var sub = await armClient.GetDefaultSubscriptionAsync();
+Console.WriteLine("Sub ID from direct DefaultAzureCredential(): " + sub.Id);
+
+armClient = host.Services.GetRequiredService<ArmClient>();
+sub = await armClient.GetDefaultSubscriptionAsync();
+Console.WriteLine("Sub ID from DI: " + sub.Id);
